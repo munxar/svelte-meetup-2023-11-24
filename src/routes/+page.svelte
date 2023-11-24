eef26fe4bd2f912e529d5ee95bc3036e58203e1d
Sascha Aeppli <sascha.aeppli@liip.ch>
2023-11-24 20:07:52 +0100

    bind vars

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
 <script lang="ts">
+	let name = 'Sascha';
 </script>
+
+<h1>Stores</h1>
+one way
+<input type="text" value={name} />
+two way
+<input type="text" bind:value={name} />
+My name is {name}.
